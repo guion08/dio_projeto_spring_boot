af56ce6faf61d774f4a67328666440e659fe2a33
guion08 <guion2717@gmail.com>
2021-08-31 12:46:30 -0300

    Add files via upload

---
+++ app/src/test/java/com/dio/projeto_spring_boot/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.projeto_spring_boot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
